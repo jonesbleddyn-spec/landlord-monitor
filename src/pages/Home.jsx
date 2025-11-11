98b03410d155030f5e8c7053b1513e344f5c8ee6
Bleddyn Jones <jonesbleddyn@gmail.com>
2025-11-11 15:36:51 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,8 @@ import {
   TrendingUp,
   CheckCircle,
   ArrowRight,
-  Building2
+  Building2,
+  AlertCircle
 } from "lucide-react";
 
 export default function Home() {
@@ -138,6 +139,16 @@ export default function Home() {
                     Sign In
                   </Button>
                 )}
+                <Link to={createPageUrl("PublicReportFault")}>
+                  <Button 
+                    size="lg" 
+                    variant="outline" 
+                    className="border-white text-white hover:bg-white/10 text-lg px-8 py-6 w-full sm:w-auto"
+                  >
+                    <AlertCircle className="mr-2 w-5 h-5" />
+                    Report Fault
+                  </Button>
+                </Link>
               </div>
             </div>
 
